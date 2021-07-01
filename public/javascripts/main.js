f92f7a5a86510e3804f62636646f0cb005023ece
donghoonlee94 <94donghoon.lee@gmail.com>
2021-07-01 14:27:52 +0900

    Add info window

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -20,6 +20,20 @@ const data = [
   },
 ];
 
+let markerList = [];
+let infoWindowList = [];
+
+const getClickHandler = (i) => () => {
+  const marker = markerList[i];
+  const infowindow = infoWindowList[i];
+  if (infowindow.getMap()) {
+    infowindow.close();
+  }
+  {
+    infowindow.open(map, marker);
+  }
+};
+
 for (let i in data) {
   const target = data[i];
   const latlng = new naver.maps.LatLng(target.lat, target.lng);
@@ -29,6 +43,28 @@ for (let i in data) {
     position: latlng,
     icon: {
       content: `<div class="marker"></div>`,
+      anchor: new naver.maps.Point(7.5, 7.5),
     },
   });
+
+  const content = `
+  <div class="info-window_wrap">
+    <div class="info-window_title">${target.title}</div>
+    <div class="info-window_address">${target.address}</div>
+  </div>
+  `;
+
+  const infoWindow = new naver.maps.InfoWindow({
+    content,
+    backgroundColor: '#00ff0000',
+    borderColor: '#00ff0000',
+    anchorSize: new naver.maps.Size(0, 0),
+  });
+
+  markerList.push(marker);
+  infoWindowList.push(infoWindow);
+}
+
+for (let i = 0, ii = markerList.length; i < ii; i++) {
+  naver.maps.Event.addListener(markerList[i], 'click', getClickHandler(i));
 }
