1986f6d50965d1271f81aab064f49e6320794a36
donghoonlee94 <94donghoon.lee@gmail.com>
2021-07-07 17:03:40 +0900

    Add postman test

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,19 @@ router.get('/upload', (req, res, next) => {
   res.render('upload');
 });
 
+router.get('/test', (req, res, next) => {
+  console.log('테스트 완료');
+  res.json({
+    message: 'res 완료',
+  });
+});
+
+router.post('/test2', (req, res, next) => {
+  const test = req.body.test;
+  console.log(test);
+  res.json({
+    message: 'post 완료',
+  });
+});
+
 module.exports = router;
